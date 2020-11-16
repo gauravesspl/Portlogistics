7b1045c47b2b157486c3d6d4dc5c91fd045fef08
saibalchakravarty <47980394+saibalchakravarty@users.noreply.github.com>
2020-11-16 15:05:33 +0530

    Update home.blade.php
    CDN SRI integrity hash added.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -211,7 +211,7 @@
 
 @endsection
 @push('script')
-<script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
+<script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js" integrity="sha256-t9UJPrESBeG2ojKTIcFLPGF7nHi2vEc7f5A2KpH/UBU=" crossorigin="anonymous"></script>
 
 <script type='text/javascript'>
     var token = '{{ csrf_token() }}';
