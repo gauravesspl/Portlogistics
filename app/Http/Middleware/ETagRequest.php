fc15191a67f7872f7c90b5aee4fba28d4df6eff8
rashiesspl <65282959+rashiesspl@users.noreply.github.com>
2020-11-16 19:17:21 +0530

    Update ETagRequest.php

--- app/Http/Middleware/ETagRequest.php
+++ app/Http/Middleware/ETagRequest.php
@@ -20,38 +20,34 @@ class ETagRequest
         {
             $request->merge(['isAjaxRequest' => true]);
         }
-        if ($request->isMethod('GET'))
+        if ($request->isMethod('GET') && !empty($request->cache_array))
         {
-            if (!empty($request->cache_array))
+            $org_id = $request->cache_array['organization_id'];
+            $user_id = $request->cache_array['user_id'];
+            $cache_key = $request->cache_array['cache_key'];
+            $key = $org_id . ':' . $user_id . ':' . $cache_key;
+            $requestDataCache = Cache::get($key);
+            $requestJson = json_encode($requestDataCache);
+            $etag = bcrypt($requestJson);
+            $requestEtag = str_replace('"', '', $request->getETags());
+            if ($requestEtag && $requestEtag[0] == $etag && !empty($requestDataCache))
             {
-                $org_id = $request->cache_array['organization_id'];
-                $user_id = $request->cache_array['user_id'];
-                $cache_key = $request->cache_array['cache_key'];
-                $key = $org_id . ':' . $user_id . ':' . $cache_key;
-                $requestDataCache = Cache::get($key);
-                $requestJson = json_encode($requestDataCache);
-                $etag = md5($requestJson);
-                $requestEtag = str_replace('"', '', $request->getETags());
-                Log::info(json_encode($requestEtag));
-                if ($requestEtag && $requestEtag[0] == $etag && !empty($requestDataCache))
+                $requestDataCache = $requestDataCache[$org_id][$user_id][$cache_key];
+                $cacheSource = $requestDataCache['cache_array']['source'];
+                $cacheView = $requestDataCache['view'];
+                $cacheData = $requestDataCache['response'];
+                $cacheData = (array)$cacheData;
+                if ($cacheSource == 'browser')
                 {
-                    $requestDataCache = $requestDataCache[$org_id][$user_id][$cache_key];
-                    $cacheSource = $requestDataCache['cache_array']['source'];
-                    $cacheView = $requestDataCache['view'];
-                    $cacheData = $requestDataCache['response'];
-                    $cacheData = (array)$cacheData;
-                    if ($cacheSource == 'browser')
-                    {
-                        header("Etag: $etag");
-                        header("HTTP/1.1 304 Not Modified");
-                        return new response(view($cacheView, $cacheData));
-                    }
-                    if ($cacheSource == 'api')
-                    {
-                        header("Etag: $etag");
-                        header("HTTP/1.1 304 Not Modified");
-                        return response()->json($cacheData, 200);
-                    }
+                    header("Etag: $etag");
+                    header("HTTP/1.1 304 Not Modified");
+                    return new response(view($cacheView, $cacheData));
+                }
+                if ($cacheSource == 'api')
+                {
+                    header("Etag: $etag");
+                    header("HTTP/1.1 304 Not Modified");
+                    return response()->json($cacheData, 200);
                 }
             }
         }
