ad847d09a821543c427e5c5ad44d310a0bdc0e7a
dasmadhusmita <48760586+dasmadhusmita@users.noreply.github.com>
2020-11-17 11:01:38 +0530

    challan pdf sonar fixes

--- resources/views/pdf/challan-pdf.blade.php
+++ resources/views/pdf/challan-pdf.blade.php
@@ -31,15 +31,15 @@
                                     <table class="column-1-2" style="border-collapse: collapse;border-bottom: 1px solid #213965; padding: 0px; width:100%; text-align: left;">
                                         <tbody>
                                             <tr>
-                                                <td style="width:25%" valign="top">
+                                                <td style="width:25%;vertical-align:top;">
                                                     <h5 style="margin: 0">Challan No:</h5>
                                                     <p style="margin: 0">{{$request['challan_no']}}</p>
                                                 </td>
-                                                <td style="text-align: center; width:50%" valign="top">
+                                                <td style="text-align: center; width:50%;vertical-align:top;">
                                                     <h1 style="margin: 0">{{$request['org_name']}}</h1>
                                                     <p>{{$request['org_address']}}</p>
                                                 </td>
-                                                <td style="width:25%" valign="top">
+                                                <td style="width:25%;vertical-align:top;">
                                                     <h5 style="margin: 0">{{$request['loaded_at']}} <br>Shift:{{$request['shift_name']}}</h5>
                                                 </td>
                                             </tr>
@@ -48,10 +48,10 @@
                                     <table class="column-1-2" style="border-collapse: collapse;width:100%; text-align: left;border: none;padding:0px;">
                                         <tbody>
                                             <tr>
-                                                <td style="width:50%" valign="top">
+                                                <td style="width:50%;vertical-align:top;">
                                                     <h5><strong>Place form: </strong>{{$request['origin']}}</h5>
                                                 </td>
-                                                <td style="width:50%;text-align:right;" valign="top">
+                                                <td style="width:50%;text-align:right;vertical-align:top;">
                                                     <h5><strong>Place to: </strong>{{$request['destination']}}</h5>
                                                 </td>
                                             </tr>
@@ -60,10 +60,10 @@
                                     <table class="column-1-2" style="border-collapse: collapse;width:100%; text-align: left;border: none;padding:0px;">
                                         <tbody>
                                             <tr>
-                                                <td style="width:50%" valign="top">
+                                                <td style="width:50%;vertical-align:top;">
                                                     <h5><strong>Vessel: </strong>{{$request['vessel_name']}}</h5>
                                                 </td>
-                                                <td style="width:50%;text-align:right;" valign="top">
+                                                <td style="width:50%;text-align:right;vertical-align:top;">
                                                     <img src="{{$request['barcode_path']}}" alt="barcode" style="width:150px;height:30px;"/>
                                                 </td>
                                             </tr>
@@ -72,20 +72,20 @@
                                     <table class="column-1-2" style="border-collapse: collapse;width:100%; text-align: left;border: none;padding:0px;">
                                         <tbody>
                                             <tr>
-                                                <td style="width:50%" valign="top">
+                                                <td style="width:50%;vertical-align:top;">
                                                     <h5><strong>Cargo: </strong>{{$request['cargo_name']}}</h5>
                                                 </td>
-                                                <td style="width:50%" valign="top"></td>
+                                                <td style="width:50%;vertical-align:top;"></td>
                                             </tr>
                                         </tbody>
                                     </table>
                                     <table class="column-1-2" style="border-collapse: collapse;width:100%; text-align: left;border: none;padding:0px;">
                                         <tbody>
                                             <tr>
-                                                <td style="width:50%" valign="top">
+                                                <td style="width:50%;vertical-align:top;">
                                                     <h5 style="margin: 0"><strong>Truck/Dumper No: </strong> {{$request['truck_no']}}</h5>
                                                 </td>
-                                                <td style="width:50%;border-top: 1px solid #213965;" valign="top">
+                                                <td style="width:50%;border-top: 1px solid #213965;vertical-align:top;">
                                                     <p style="text-align:center;width:100%;"><strong>Signature</strong></p>
                                                 </td>
                                             </tr>
